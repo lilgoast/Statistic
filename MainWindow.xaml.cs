f338582b1ecbba80dfdd678aa98def5c703d391c
lilgoast <105431041+lilgoast@users.noreply.github.com>
2022-09-12 02:25:23 +0300

    Code refactoring

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -32,6 +32,49 @@ namespace Statistic
             InitializeComponent();
         }
 
+<<<<<<< Updated upstream
+=======
+        //--------------ComboBox Handler--------------
+
+        //Intervals
+        private void comboBoxIntervals_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<string> data = new List<string>();
+            data.Add("Период"); //indx = 0
+            data.Add("Разделить на"); //indx = 1
+
+            var combo = sender as ComboBox;
+            combo.ItemsSource = data;
+            combo.SelectedIndex = 0;
+        }
+
+        private void comboBoxIntervals_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedComboItem = sender as ComboBox;
+            chosedIntervals = selectedComboItem.SelectedIndex;
+        }
+
+        //Series
+        private void comboBoxSeries_Loaded(object sender, RoutedEventArgs e)
+        {
+            List<string> data = new List<string>();
+            data.Add("Ряд частот");  //indx = 0
+            data.Add("Ряд относительных частот"); //indx = 1
+
+            var combo = sender as ComboBox;
+            combo.ItemsSource = data;
+            combo.SelectedIndex = 0;
+        }
+
+        private void comboBoxSeries_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var selectedComboItem = sender as ComboBox;
+            chosedSeries = selectedComboItem.SelectedIndex;
+        }
+
+        //--------------Text In "periodTextBox" Handler--------------
+
+>>>>>>> Stashed changes
         private void PreviewTextInput(object sender, TextCompositionEventArgs e)
         {
             Regex regex = new Regex("[^0-9]+");
@@ -82,15 +125,14 @@ namespace Statistic
                 }
                 numsTextBox.Text = nums;
             }
-            Button_Click_1(sender, e);
-            Button_Click_2(sender, e);
+            Button_Click_Series(sender, e);
         }
 
-        //--------------Variational series--------------
+        //--------------Build Series Button Handler--------------
 
-        private void Button_Click_1(object sender, RoutedEventArgs e)
+        private void Button_Click_Series(object sender, RoutedEventArgs e)
         {
-            double[] amountOfNumbers = new double[1];
+            double[] amountOfNumbers = new double[0];
             string line;
             string nums = numsTextBox.Text.ToString();
             StringReader strReader = new StringReader(nums);
@@ -113,6 +155,8 @@ namespace Statistic
 
             double[,] numbers = new double[amountOfNumbers.Distinct().ToArray().Length - 1, 2];
             Array.Sort(amountOfNumbers);
+            double minValue = amountOfNumbers.Min();
+            double maxValue = amountOfNumbers.Max();
             int index = 0;
             int arrayLength = amountOfNumbers.Distinct().ToArray().Length - 1;
 
@@ -135,11 +179,18 @@ namespace Statistic
                 }
             }
 
+<<<<<<< Updated upstream
             //Grid1
             DataGridTextColumn[] textColumn1 = new DataGridTextColumn[arrayLength + 1];
             Row1 row1 = new Row1();
             row1.numbers = new int[arrayLength];
             double amountOfAllNums = 0;
+=======
+            //variational series
+
+            DataGridSeries.Columns.Clear();
+            DataGridSeries.Items.Clear();
+>>>>>>> Stashed changes
 
             DataGrid1.Columns.Clear();
             DataGrid1.Items.Clear();
@@ -189,6 +240,7 @@ namespace Statistic
                 row2.numbers[i - 1] = Math.Round((Convert.ToInt32(numbers[i - 1, 1]) + 1) / amountOfAllNums, 2);
             }
 
+<<<<<<< Updated upstream
             row2.name = "W";
 
             DataGrid2.Items.Add(row2);
@@ -255,45 +307,35 @@ namespace Statistic
             double maxValue = amountOfNumbers.Max();
             int index = 0;
             int arrayLength = amountOfNumbers.Distinct().ToArray().Length - 1;
+=======
+            DataGridSeries.Items.Add(row);
+>>>>>>> Stashed changes
 
-            for (int i = 0; i < amountOfNumbers.Length; i++)
-            {
-                bool numberFinded = false;
-                for (int d = 0; d < arrayLength; d++)
-                {
-                    if (numbers[d, 0] == amountOfNumbers[i])
-                    {
-                        numbers[d, 1] += 1;
-                        numberFinded = true;
-                        break;
-                    }
-                }
-                if (!numberFinded)
-                {
-                    numbers[index, 0] = amountOfNumbers[i];
-                    index++;
-                }
-            }
+            //intervals
 
             DataGridTextColumn[] textColumnIntervals = new DataGridTextColumn[arrayLength + 1];
             RowIntervals rowIntervals = new RowIntervals();
             rowIntervals.numbers = new double[arrayLength];
             double amountOfAllNums = 0;
 
-            DataGrid3.Columns.Clear();
-            DataGrid3.Items.Clear();
+            DataGridIntervals.Columns.Clear();
+            DataGridIntervals.Items.Clear();
 
             DataGridTextColumn tcIntervals = new DataGridTextColumn();
             tcIntervals.Header = "Xi";
             tcIntervals.Binding = new Binding("name");
-            DataGrid3.Columns.Add(tcIntervals);
+            DataGridIntervals.Columns.Add(tcIntervals);
 
-            for (int i = 0; i <= arrayLength; i++)
+            for (int i = 0; i < arrayLength; i++)
             {
                 amountOfAllNums += Convert.ToInt32(numbers[i, 1]) + 1;
             }
 
+<<<<<<< Updated upstream
             if (chosed == 0) // Период
+=======
+            if (chosedIntervals == 0) //period
+>>>>>>> Stashed changes
             {
                 periodLabel.Content = "";
                 int period = Convert.ToInt32(periodTextBox.Text);
@@ -310,9 +352,9 @@ namespace Statistic
                     textColumnIntervals[i + 1] = new DataGridTextColumn();
                     textColumnIntervals[i + 1].Header = $"[{Math.Round(iterationValue, 2)}, {Math.Round(iterationValue + period, 2)}) ({Math.Round(absoluteInterval, 2)})";
                     textColumnIntervals[i + 1].Binding = new Binding($"numbers[{i}]");
-                    DataGrid3.Columns.Add(textColumnIntervals[i + 1]);
+                    DataGridIntervals.Columns.Add(textColumnIntervals[i + 1]);
 
-                    while (indexPeriod <= arrayLength 
+                    while (indexPeriod < arrayLength
                         && numbers[indexPeriod, 0] >= iterationValue
                         && numbers[indexPeriod, 0] < (double)iterationValue + period)
                     {
@@ -325,7 +367,11 @@ namespace Statistic
                     i++;
                 }
             }
+<<<<<<< Updated upstream
             else if(chosed == 1) // Разделить на
+=======
+            else if (chosedIntervals == 1) //devided
+>>>>>>> Stashed changes
             {
                 double period = (maxValue - minValue) / Convert.ToDouble(periodTextBox.Text);
                 periodLabel.Content = "Период: " + Math.Round(period, 2);
@@ -333,14 +379,15 @@ namespace Statistic
                 double iterationValue = minValue;
                 double absoluteInterval;
                 int indexPeriod = 0;
+                int iterationAmount = Convert.ToInt32(periodTextBox.Text);
 
-                while (Math.Round(iterationValue, 5) < maxValue)
+                while (iterationAmount > 0)
                 {
                     int periodValue = 0;
 
                     absoluteInterval = ((double)iterationValue * 2 + period) / 2;
                     textColumnIntervals[i + 1] = new DataGridTextColumn();
-                    if(iterationValue == minValue || iterationValue >= maxValue - period)
+                    if (iterationValue == minValue || iterationValue >= maxValue - period)
                     {
                         textColumnIntervals[i + 1].Header = $"[{Math.Round(iterationValue, 2)}, {Math.Round(iterationValue + period, 2)}] ({Math.Round(absoluteInterval, 2)})";
                     }
@@ -349,9 +396,9 @@ namespace Statistic
                         textColumnIntervals[i + 1].Header = $"({Math.Round(iterationValue, 2)}, {Math.Round(iterationValue + period, 2)}] ({Math.Round(absoluteInterval, 2)})";
                     }
                     textColumnIntervals[i + 1].Binding = new Binding($"numbers[{i}]");
-                    DataGrid3.Columns.Add(textColumnIntervals[i + 1]);
+                    DataGridIntervals.Columns.Add(textColumnIntervals[i + 1]);
 
-                    while (indexPeriod <= arrayLength
+                    while (indexPeriod < arrayLength
                         && numbers[indexPeriod, 0] >= iterationValue
                         && numbers[indexPeriod, 0] <= Math.Round((double)iterationValue + period, 5))
                     {
@@ -362,12 +409,29 @@ namespace Statistic
                     rowIntervals.numbers[i] = periodValue;
                     iterationValue += period;
                     i++;
+                    iterationAmount--;
                 }
             }
             rowIntervals.name = "Ni(Pi)";
 
-            DataGrid3.Items.Add(rowIntervals);
+            DataGridIntervals.Items.Add(rowIntervals);
         }
 
+<<<<<<< Updated upstream
+=======
+        //Class for series columns
+        public class Row
+        {
+            public double[] numbers { get; set; }
+            public string name { get; set; }
+        }      
+        
+        //Interval's class for columns
+        public class RowIntervals
+        {
+            public double[] numbers { get; set; }
+            public string name { get; set; }
+        }
+>>>>>>> Stashed changes
     }
 }
